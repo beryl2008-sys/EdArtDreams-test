f5a3b16974248e165fab279af75104a92dfd7a1d
beryl2008-sys <82329576+beryl2008-sys@users.noreply.github.com>
2024-04-11 18:51:25 +0530

    u

---
+++ EdArtDreams Web/indexconfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAB6HApfvw-QioqR99RiliAS1mK2uDMaWE",
+  authDomain: "edartdreams-20373.firebaseapp.com",
+  databaseURL: "https://edartdreams-20373-default-rtdb.firebaseio.com",
+  projectId: "edartdreams-20373",
+  storageBucket: "edartdreams-20373.appspot.com",
+  messagingSenderId: "547805622683",
+  appId: "1:547805622683:web:d64a1f62988118b475713e"
+};
+export {firebaseConfig};
