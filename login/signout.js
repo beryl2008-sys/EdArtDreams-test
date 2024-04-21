c64c912227f674e85399abe1f006ca43deaf54c3
beryl2008-sys <82329576+beryl2008-sys@users.noreply.github.com>
2024-04-21 10:01:28 +0530

    u

--- login/signout.js
+++ login/signout.js
@@ -1,4 +1,4 @@
-import { firebaseConfig } from "./indexconfig.js";
+import { firebaseConfig } from "./loginconfig.js";
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const signoutBtn = document.querySelector('#signoutbtn');
